92914579e0570f03116887eda36e35b6e3aebcd3
ghwns40@naver.com <ghwns40@naver.com>
2023-02-08 12:15:37 +0900

    프론트 변경

---
+++ public/js/goodsdetail.js
@@ -0,0 +1,17 @@
+const addcart = () => {
+  const data = {
+    goodsId: "1",
+    quanitity: "1",
+  };
+  $.ajax({
+    type: "POST",
+    url: "/api/user/cart",
+    data: data,
+    success: function (response) {
+      console.log(response);
+      if (response) {
+        location.href = "/api/cart";
+      }
+    },
+  });
+};
